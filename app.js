36b9203dd860c6e01d0ee0a0ccd3241db8c9e1fa
BriceWIKTOR <brice.ark@gmail.com>
2024-09-06 12:32:16 +0200

    squelette html  + debut js

---
+++ app.js
@@ -0,0 +1,6 @@
+const newTitle = document.getElementById("newTitleInput");
+const newText = document.getElementById("newTextInput");
+const createBtn = document.getElementById("createBtn");
+const notesContainer = document.getElementById("notesListContainer");
+
+createBtn.addEventListener("click");
