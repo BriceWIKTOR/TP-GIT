9e3b62bb31c0f396aa2bf9c7c31e5f950c75c896
AlexCourivaud <alexandre.courivaud@gmail.com>
2024-09-06 14:03:50 +0200

    ajout du set localStorage

--- app.js
+++ app.js
@@ -3,4 +3,19 @@ const newText = document.getElementById("newTextInput");
 const createBtn = document.getElementById("createBtn");
 const notesContainer = document.getElementById("notesListContainer");
 
-createBtn.addEventListener("click");
+
+createBtn.addEventListener("click", addNote);
+
+function addNote() {
+    let title = newTitle.value;
+    let text = newText.value;
+    var noteObject = {
+        title: title,
+        text: text,
+    };
+    var noteArrays = [];
+    noteArrays.push(noteObject);
+    localStorage.setItem("Notes", JSON.stringify(noteArrays));
+    
+    // alert("vous avez ajouter la note" + title);
+}
