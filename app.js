1e531688d4692ab200d131d8ff983b2c616d82e5
BriceWIKTOR <brice.ark@gmail.com>
2024-09-06 15:15:57 +0200

    css + ajout js

--- app.js
+++ app.js
@@ -3,19 +3,30 @@ const newText = document.getElementById("newTextInput");
 const createBtn = document.getElementById("createBtn");
 const notesContainer = document.getElementById("notesListContainer");
 
-
 createBtn.addEventListener("click", addNote);
 
 function addNote() {
-    let title = newTitle.value;
-    let text = newText.value;
-    var noteObject = {
-        title: title,
-        text: text,
-    };
-    var noteArrays = [];
+  let title = newTitle.value;
+  let text = newText.value;
+  var noteObject = {
+    title: title,
+    text: text,
+  };
+  var noteArrays = [];
+
+  if (localStorage.getItem("notes")) {
+    noteArrays.push(JSON.parse(localStorage.getItem("notes")));
+  } else {
     noteArrays.push(noteObject);
-    localStorage.setItem("Notes", JSON.stringify(noteArrays));
-    
-    // alert("vous avez ajouter la note" + title);
+  }
+  localStorage.setItem("notes", JSON.stringify(noteArrays));
+  // alert("vous avez ajouter la note" + title);
 }
+
+function displayNote() {
+  const parseNote = JSON.parse(localStorage.getItem("notes"));
+
+  console.log(parseNote);
+}
+
+displayNote();
